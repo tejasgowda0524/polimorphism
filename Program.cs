e4d0d82501915bd2ba1bd65c0ff50b752b8f0c1d
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 20:46:11 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿namespace newProj
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            plane pl = new plane();
+            passengerPlane pas = new passengerPlane();
+            cargoPlane cargo = new cargoPlane();
+            fighterPlane figh = new fighterPlane();
+
+            /*plane p = pas;
+            pas.land();
+            pas.fly();
+            pas.carryPassenger();
+            p = cargo;
+            cargo.land();
+            cargo.fly();
+            cargo.carryCargo();
+            p=figh;
+            figh.land();
+            figh.fly();
+            figh.carryWeapons();*/
+
+
+            /*pas.fly();
+           pas.land();
+           pas.carryPassenger();
+           cargo.fly();
+           cargo.land();
+           cargo.carryCargo();
+           figh.fly();
+           figh.land();
+           figh.carryWeapons();*/
+
+            Permit(pl);
+            Permit(pas);
+            Permit(cargo);
+            Permit(figh);
+
+            Console.ReadLine();
+
+
+
+
+        }
+        static void Permit(plane p)
+        {
+            p.fly();
+            p.land();
+
+
+
+            if (p is passengerPlane)
+            {
+                ((passengerPlane)p).carryPassenger();
+            }
+
+            // Check if the object is a cargoPlane, and if so, call carryCargo
+            if (p is cargoPlane)
+            {
+                ((cargoPlane)p).carryCargo();
+            }
+
+            // Check if the object is a fighterPlane, and if so, call carryWeapons
+            if (p is fighterPlane)
+            {
+                ((fighterPlane)p).carryWeapons();
+            }
+        }
+    }
+}
---
+++ plane.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newProj
+{
+    internal class plane
+    {
+        public void fly()
+        {
+            Console.WriteLine("plane is flying ");
+        }
+        public void land()
+        {
+            Console.WriteLine("plane is landing");
+        }
+       
+    }
+    internal class passengerPlane() : plane
+    {
+        public void carryPassenger()
+        {
+            Console.WriteLine("passenger plane carry passengers");
+        }
+        public void fly()
+        {
+            Console.WriteLine("passengerplane is flying at medium Levels");
+        }
+
+    }
+    internal class cargoPlane() : plane
+    {
+        public void carryCargo()
+        {
+            Console.WriteLine("cargo plane carry goods");
+        }
+        public void fly()
+        {
+            Console.WriteLine("cargoplane is flying at low Levels");
+        }
+
+    }
+    internal class fighterPlane() : plane
+    {
+        public void carryWeapons()
+        {
+            Console.WriteLine("fighter plane carry weapons");
+        }
+        public void fly()
+        {
+            Console.WriteLine("fighterplane is flying at high Levels");
+        }
+
+    }
+   
+}
